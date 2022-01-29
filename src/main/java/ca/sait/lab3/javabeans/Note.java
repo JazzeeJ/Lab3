190b35e93b5e8695819548fbccb0b94d35d0064f
JazzeeJ <jasonleesienyang@gmail.com>
2022-01-29 15:16:47 -0700

    Initial javabeans,servlets, and jsp files created

--- src/main/java/ca/sait/lab3/javabeans/Note.java
+++ src/main/java/ca/sait/lab3/javabeans/Note.java
@@ -3,9 +3,32 @@ package ca.sait.lab3.javabeans;
 import java.io.Serializable;
 
 /**
- *
+ * Represents a Note
  * @author Jason Lee
  */
 public class Note implements Serializable {
-    HIIII joe blow
+    private String title;
+    private String contents;
+    
+    public Note() {
+        this.title = "";
+        this.contents = "";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    
 }
--- src/main/webapp/WEB-INF/viewnote.jsp
+++ src/main/webapp/WEB-INF/viewnote.jsp
@@ -9,9 +9,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>View Note</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>View Note</h1>
+        
+        <p>Title: </p>
+        <p>Contents: </p>
+        
+        <a href="#">Edit</a>
     </body>
 </html>
